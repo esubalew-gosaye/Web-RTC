dc6eef444d5df6516e5cdd62e6f58eeebebb8dc3
esubalew-gosaye <mesaye2010@gmail.com>
2023-03-27 10:50:43 +0300

    room id generate added.

--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-
+import random
 # Create your views here.
 
 def index(request):
-    return render(request, 'chat/index.html')
+    context = {}
+    if request.method == 'GET':
+        if request.GET.get('q') == 'room_id':
+            # Generate a random string of 8 characters
+            room_id = ''.join(random.sample('ABCDEFGHIJKLMNOPQRSTUVWXYZabc', k=8))
+            context['room_id'] = room_id
+    return render(request, 'chat/index.html', context=context)
+
+def room(request):
+    return render(request, 'chat/room.html')
